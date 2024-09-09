bfc1520292e814f2e642c59aa4de1ce42a3ce82e
Johnatan Rodrigues <johndriguess@gmail.com>
2024-09-09 00:15:26 -0300

    login and register

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,9 +1,10 @@
 from . import views
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('users', views.get_users, name='get_all_users'),
     path('users/<str:cpf>', views.get_by_cpf),
     path('data/', views.user_manager),
+    path('register/', views.register_user, name='register_user'),  
+    path('login/', views.login_user, name='login_user'),  
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.hashers import check_password
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -18,7 +19,7 @@ def get_users(request):
         return Response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def get_by_cpf(request, cpf):
     try:
         user = User.objects.get(pk=cpf)
@@ -28,20 +29,94 @@ def get_by_cpf(request, cpf):
     if request.method == 'GET':
         serializer = UserSerializer(user)
         return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)   
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def user_manager(request):
     if request.method == 'GET':
-        try: 
-            if request.GET['user']:
-                user_cpf = request.GET['user']
+        try:
+            if request.GET['user']:                     
+                user_cpf = request.GET['user'] 
                 try:
-                    user = User.objects.get(pk=cpf)
+                    user = User.objects.get(pk=user_cpf) 
                 except:
                     return Response(status=status.HTTP_404_NOT_FOUND)
-                serializer = UserSerializer(user)
-                return Response(serializer.data)
+
+                serializer = UserSerializer(user)          
+                return Response(serializer.data)       
+
             else:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
+            
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    if request.method == 'POST':
+        new_user = request.data
+        serializer = UserSerializer(data=new_user)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'PUT':
+        user_cpf = request.data['user_cpf'] 
+        try:
+            user_to_update = User.objects.get(pk=user_cpf) 
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(user_to_update, data=request.data)   
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+    if request.method == 'DELETE':
+        try:
+            user_to_delete = User.objects.get(pk=request.data['user_cpf'])
+            user_to_delete.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User(
+                user_name=serializer.validated_data['user_name'],
+                user_cpf=serializer.validated_data['user_cpf'],
+                user_email=serializer.validated_data['user_email'],
+                user_age=serializer.validated_data['user_age']
+            )
+            user.set_password(serializer.validated_data['user_password'])
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['POST'])
+def login_user(request):
+    if request.method == 'POST':
+        cpf = request.data.get('user_cpf')
+        password = request.data.get('user_password')
+        
+        try:
+            user = User.objects.get(user_cpf=cpf)
+            if user.check_password(password):
+                return Response({"message": "Login bem-sucedido", "user": UserSerializer(user).data}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Senha incorreta"}, status=status.HTTP_400_BAD_REQUEST)
+        except User.DoesNotExist:
+            return Response({"message": "Usuário não encontrado"}, status=status.HTTP_404_NOT_FOUND)
